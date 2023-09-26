cabf7824d6d0e2523780250cde4c8837cbbfb8d7
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-09-26 13:36:17 +0300

    linked "register method" to the account-service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -25,6 +25,18 @@ export class AccountService {
     )
   }
 
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register',model).pipe(
+      map(user => {
+        if (user){
+          localStorage.setItem('user',JSON.stringify(user))
+          this.CurrentUserSource.next(user);
+        }
+        return user;
+      })
+    )
+  }
+
   setCurrentUser(user: User){
     this.CurrentUserSource.next(user);
   }

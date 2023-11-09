fa23d810b285564cd6c00d1b1d2e0c1860fefd6b
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-11-10 01:51:15 +0200

    improved classes + dbcontext + added migration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,7 +19,7 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new();      
-        public List<AppUser> LikedByUsers { get; set; }
-        public List<AppUser> LikedUsers { get; set; }
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
     }
 }

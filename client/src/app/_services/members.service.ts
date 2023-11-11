84fc7ad0eb870a30ba90d2eff859d209c0b9657d
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-11-11 21:36:41 +0200

    added likes component

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -126,6 +126,6 @@ export class MembersService {
   }
 
   getLikes(predicate: string){
-    return this.http.get(this.baseUrl + 'likes?predicate=' + predicate);
+    return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
   }
 }

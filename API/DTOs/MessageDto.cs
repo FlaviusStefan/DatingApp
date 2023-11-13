9ff469e3b77e20835d9ebe74e4f6a129b588b0dc
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-11-13 13:23:35 +0200

    msgctrl + fixed msgdto props + serviceextnsn

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -8,6 +8,8 @@ namespace API.DTOs
         public int SenderId { get; set; }
         public string SenderUsername { get; set; }
         public string SenderPhotoUrl { get;set; }
+        public int RecipientId { get; set; }
+        public string RecipientUsername { get; set; }
         public string RecipientPhotoUrl{get;set;}
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }

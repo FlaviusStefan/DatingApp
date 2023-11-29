a7f0653e8ab2a134b9a3b8a171dfe3c7cdb0cc0a
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-11-29 19:49:29 +0200

    edit the roles in the client

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -7,14 +7,18 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit{
+username = '';
+availableRoles: any[] = [];
+selectedRoles: any[] = [];
 
-  title = '';
-  list: any;
-  closeBtnName = '';
 
   constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
   }
 
+  updateChecked(checkedValue: string) {
+    const index = this.selectedRoles.indexOf(checkedValue);
+    index !== -1 ? this.selectedRoles.splice(index, 1) : this.selectedRoles.push(checkedValue);
+  }
 }

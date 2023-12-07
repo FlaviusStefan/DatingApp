1a2a1ec1a681e95c22f942533729a813d65983b0
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-12-07 19:16:48 +0200

    prepared the client + serving from API server

--- API/Program.cs
+++ API/Program.cs
@@ -27,6 +27,9 @@ app.UseCors(builder => builder
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
